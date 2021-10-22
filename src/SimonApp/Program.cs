6f403575f0949910f40bf683b4210f0ce049e21b
Mikeo Skinner <mikeokskinner@outlook.com>
2021-10-22 14:47:02 -0500

    Added methods for greeting, timer and random number logic

--- src/SimonApp/Program.cs
+++ src/SimonApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SimonApp
@@ -10,6 +11,50 @@ namespace SimonApp
     {
         static void Main(string[] args)
         {
+            Greeting();
+            Console.ReadKey();
+            Console.Clear();
+            int randNum = Random();
+            Timer();
+            int userNum = User();
+            bool isCheck = Check(randNum, userNum);
+            Console.WriteLine(isCheck);
+            Console.WriteLine("Game over");
+        }
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to the Simon Game");
+            Console.WriteLine("Press any key to play");
+        }
+        static int Random()
+        {
+            Random randNum = new Random();
+            int simonNum = randNum.Next(0, 9);
+            Console.WriteLine(simonNum);
+            return simonNum;
+        }
+        static void Timer()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        static bool Check(int randNum, int userNum)
+        {
+            bool isMatch = true;
+            if (randNum == userNum)
+            {
+                isMatch = true;
+            }
+            else
+            {
+                isMatch = false;
+            }
+            return isMatch;
+        }
+        static int User()
+        {
+            int userNum = Console.Read();
+            return userNum;
         }
     }
 }
