ddc662f4c915058e02081a49a4bfd83dd37b0ca7
Mikeo Skinner <mikeokskinner@outlook.com>
2021-10-22 15:37:32 -0500

    Working on repeating logic and added random numbers to list

--- src/SimonApp/Program.cs
+++ src/SimonApp/Program.cs
@@ -17,21 +17,35 @@ namespace SimonApp
             int randNum = Random();
             Timer();
             int userNum = User();
+            int counter = 0;
             bool isCheck = Check(randNum, userNum);
-            Console.WriteLine(isCheck);
-            Console.WriteLine("Game over");
+            do
+            {
+                Console.Clear();
+                randNum = Random();
+                Timer();
+                userNum = User();
+                isCheck = Check(randNum, userNum);
+                counter++;
+            }
+            while (isCheck);
+            Console.WriteLine("Game over! Your score is " + counter);
+
         }
         static void Greeting()
         {
             Console.WriteLine("Welcome to the Simon Game");
             Console.WriteLine("Press any key to play");
         }
-        static int Random()
+        static List<int> Random()
         {
             Random randNum = new Random();
-            int simonNum = randNum.Next(0, 9);
-            Console.WriteLine(simonNum);
-            return simonNum;
+            List<int> randomNumbers = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                randomNumbers.Add(randNum.Next(0, 9));
+            }
+            return randomNumbers;
         }
         static void Timer()
         {
